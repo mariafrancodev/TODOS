8e3d0694c32bfebef935244edd00110dd110f1ed
Maria Franco <mafa1008@gmail.com>
2023-10-15 12:29:12 -0500

    app base

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { TodoCounter } from './TodoCounter';
+import{ TodoSearch } from './TodoSearch';
+import{ TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+import './App.css';
+import React from 'react';
+
+const defaultTodos = [
+  {text: 'Cortar cebolla', completed: true},
+  {text: 'Tomar el curso de intro a React', completed: false},
+  {text: 'Llorar con la llorona', completed: false},
+];
+
+function App() {
+  return (
+    <React.Fragment>
+
+      <TodoCounter completed={16} total={25}/>
+      <TodoSearch />
+
+      <TodoList>
+        {defaultTodos.map(todo => (
+          <TodoItem 
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+          />
+        ))}
+      </TodoList>
+
+      <CreateTodoButton />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/TodoCounter.js
@@ -0,0 +1,9 @@
+function TodoCounter( {total, completed} ){
+    return (
+        <h2>
+            Has completado {completed} de {total} TODOs 
+        </h2>
+    );
+}
+
+export { TodoCounter };
