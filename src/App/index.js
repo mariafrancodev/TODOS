ece4f6e66165a495ae25f54a8648977205031b39
Maria Franco <mafa1008@gmail.com>
2023-11-01 11:17:34 -0500

    refact app/index

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import { TodoCounter } from '../TodoCounter';
-import{ TodoSearch } from '../TodoSearch';
-import{ TodoList } from '../TodoList';
-import { TodoItem } from '../TodoItem';
-import { CreateTodoButton } from '../CreateTodoButton';
+import { AppUI } from './AppUI';
 import './App.css';
 import  { useLocalStorage } from './useLocalStorage';
 
@@ -55,31 +51,16 @@ function App() {
     saveTodos(newTodos);
   };
 
-  return (
-    <>
-
-      <TodoCounter
-      completed={completedTodos} 
-      total={totalTodos}/>
-      <TodoSearch 
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-      />
-
-      <TodoList>
-        {searchedTodos.map(todo => (
-          <TodoItem 
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}
-          />
-        ))}
-      </TodoList>
-
-      <CreateTodoButton />
-    </>
+  return(
+    <AppUI
+      completedTodos = {completedTodos}
+      totalTodos = {totalTodos}
+      searchValue = {searchValue}
+      setSearchValue = {setSearchValue}
+      searchedTodos = {searchedTodos}
+      completeTodo = {completeTodo}
+      deleteTodo = {deleteTodo}
+    />
   );
 }
 
